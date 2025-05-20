f6c69ecba95edc28a74c9e5a792adf74eb7d4566
KimJeeHoon <rwrwg159@gmail.com>
2025-05-20 10:55:17 +0900

    temp

--- Assets/SavePlayerBasicStat.cs
+++ Assets/SavePlayerBasicStat.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Fusion;
 using UnityEngine;
 
-public class SavePlayerBasicStat : MonoBehaviour
+public class SavePlayerBasicStat : NetworkBehaviour
 {
     
     public string Email;
     public string Password;
     public string Nickname;
+    
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
     }
     
-    
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+    public void RPC_SendNicknameToServer(string nickname, RpcInfo info = default)
+    {
+        PlayerRef playerRef = info.Source;
+
+        // 서버에서 등록
+        if (BasicSpawner.Instance != null)
+        {
+            BasicSpawner.Instance.ReceiveNicknameFromClient(playerRef, nickname);
+        }
+    }
 }
