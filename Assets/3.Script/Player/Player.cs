f73f7385b28d3e1a864bdf2bfc42be92542ab3b7
yunnozee <74864602+sksdbswl@users.noreply.github.com>
2025-05-20 14:14:54 +0900

    modify card data

--- Assets/3.Script/Player/Player.cs
+++ Assets/3.Script/Player/Player.cs
@@ -13,22 +13,26 @@ public class Player : NetworkBehaviour
     public PlayerBasicStat BasicStat => playerBasicStat;
     
     [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
-    public void RPC_ReceiveHandCards(int[] cardIDs, RpcInfo info = default)
+    public void RPC_ReceiveHandCards(RpcInfo info = default)
     {
-        // 카드 할당
-        var cards = new CardData[cardIDs.Length];
-        for (int i = 0; i < cardIDs.Length; i++)
-        {
-            cards[i] = CardUIManager.Instance.GetCardByID(cardIDs[i]);
-        }
-        
-        GameStat.InGameStat.HandCards = cards;
         PlayerRef myRef = Runner.LocalPlayer;
         Debug.Log($"myRef:: {myRef}");
-
         var playerObj = Runner.GetPlayerObject(myRef);
         var player = playerObj.GetComponent<Player>();
+        var cards = new CardData[player.GameStat.InGameStat.HandCards.Length];
+        
         Debug.Log(player.BasicStat.nickName);
+        
+        // 카드 할당
+        if (Runner.IsServer)
+        {
+            for (int i = 0; i < player.GameStat.InGameStat.HandCards.Length; i++)
+            {
+                cards[i] = CardUIManager.Instance.GetCardByID(player.GameStat.InGameStat.HandCardsId[i]);
+            }
+        
+            GameStat.InGameStat.HandCards = cards;
+        }
         
         CardUIManager.Instance.UpdateHandCardUI(cards);
     }
