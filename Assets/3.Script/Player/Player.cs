6de5d3a3fe366d1cb4bdb2bdb5ba988d487ca972
yunnozee <74864602+sksdbswl@users.noreply.github.com>
2025-05-20 13:54:24 +0900

    player card ui change

--- Assets/3.Script/Player/Player.cs
+++ Assets/3.Script/Player/Player.cs
@@ -12,7 +12,6 @@ public class Player : NetworkBehaviour
     public PlayerGameStat GameStat => playerGameStat;
     public PlayerBasicStat BasicStat => playerBasicStat;
     
-    
     [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
     public void RPC_ReceiveHandCards(int[] cardIDs, RpcInfo info = default)
     {
@@ -31,47 +30,6 @@ public class Player : NetworkBehaviour
         var player = playerObj.GetComponent<Player>();
         Debug.Log(player.BasicStat.nickName);
         
-        //RPC_RequestPlayerInfo();
-        //RPC_RequestPlayerInfo_Fallback(Runner.LocalPlayer);
-    }
-    
-        
-    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
-    public void RPC_RequestPlayerInfo_Fallback(PlayerRef requester)
-    {
-        if (BasicSpawner.Instance.spawnedPlayers.TryGetValue(requester, out var playerObj))
-        {
-            var player = playerObj.GetComponent<Player>();
-            string nickname = player.BasicStat.nickName;
-
-            Debug.Log(nickname);
-            //RPC_ReceivePlayerInfo(nickname, requester);
-        }
-    }
-    
-    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
-    public void RPC_RequestPlayerInfo(RpcInfo info = default)
-    {
-        if (!Runner.IsServer) return; 
-
-        // if (info.Source == null)
-        // {
-        //     Debug.LogWarning("info.Source is null. Check who is calling this RPC.");
-        //     return;
-        // }
-
-        Debug.Log($"Runner Is Server :: {BasicSpawner.Instance.spawnedPlayers}");
-        
-        if (BasicSpawner.Instance.spawnedPlayers.TryGetValue(info.Source, out var playerObj))
-        {
-            var player = playerObj.GetComponent<Player>();
-            string nickname = player.BasicStat.nickName;
-
-            //RPC_ReceivePlayerInfo(nickname, info.Source);
-        }
-        else
-        {
-            Debug.LogWarning($"PlayerRef {info.Source} not found in spawnedPlayers.");
-        }
+        CardUIManager.Instance.UpdateHandCardUI(cards);
     }
 }
