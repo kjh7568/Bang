44cb204a7b499a1e82efb40c2653a8cdab875d51
dopajac <154491661+dopajac@users.noreply.github.com>
2025-05-21 12:47:28 +0900

    test용 깃

--- Assets/3.Script/Manager/GameManager.cs
+++ Assets/3.Script/Manager/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-       // CachePlayerInfo();
+       CachePlayerInfo();
         
         SetPlayerHuman();
         SetPlayerJob();
