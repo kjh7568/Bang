d50341786238e84e4d13b6f215c93330e9dfd224
dopajac <154491661+dopajac@users.noreply.github.com>
2025-05-27 15:20:53 +0900

    작업중 임의 저장

--- Assets/3.Script/Manager/BattleTest.cs
+++ Assets/3.Script/Manager/BattleTest.cs
@@ -7,60 +7,60 @@ using Random = UnityEngine.Random;
 
 public class BattleTest : NetworkBehaviour
 {
-    // [SerializeField] private VictoryCheck victoryCheck; // 승리 조건 체크용
-    // [SerializeField] private TMP_Text NotifyText; // 전투 결과 텍스트 UI
-    //     
-    // private void Update()
-    // {
-    //     if (!HasStateAuthority) return; 
-    //     
-    //     if (Input.GetKeyDown(KeyCode.Space))
-    //     {
-    //         ApplyRandomDamage();
-    //         string nick = GameManager.Instance.players[Random.Range(0, 4)].BasicStat.nickName;
-    //         RPC_ShowNotifyText(nick);
-    //         victoryCheck.CheckVictoryConditions();
-    //     }
-    // }
-    //
-    //
-    // [Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.All)]
-    // private void RPC_ShowNotifyText(string nickname)
-    // {
-    //     NotifyText.text = $"{nickname} 이(가) 전투를 시작했습니다.";
-    //     NotifyText.gameObject.SetActive(true);
-    // }
-    //
-    // private void ApplyRandomDamage()
-    // {
-    //     List<Player> players = GameManager.Instance.players;
-    //
-    //     if (players.Count == 0) return;
-    //
-    //     // 살아있는 플레이어만 필터링
-    //     List<Player> alivePlayers = players.FindAll(p => !p.GameStat.InGameStat.IsDead);
-    //
-    //     if (alivePlayers.Count == 0)
-    //     {
-    //         Debug.Log("모든 플레이어가 사망했습니다.");
-    //         return;
-    //     }
-    //
-    //     // 무작위 플레이어 선택
-    //     int randomIndex = Random.Range(0, alivePlayers.Count);
-    //     Player selectedPlayer = alivePlayers[randomIndex];
-    //
-    //     // 데미지 적용
-    //     selectedPlayer.GameStat.InGameStat.hp--;
-    //
-    //     Debug.Log($"{selectedPlayer.BasicStat.nickName} 이(가) 데미지를 입었습니다. 남은 체력: {selectedPlayer.GameStat.InGameStat.hp}");
-    //
-    //     // 사망 판정
-    //     if (selectedPlayer.GameStat.InGameStat.hp <= 0)
-    //     {
-    //         selectedPlayer.GameStat.InGameStat.hp = 0;
-    //         Debug.Log($"{selectedPlayer.BasicStat.nickName} 이(가) 사망했습니다.");
-    //     }
-    // }
+    [SerializeField] private VictoryCheck victoryCheck; // 승리 조건 체크용
+    [SerializeField] private TMP_Text NotifyText; // 전투 결과 텍스트 UI
+        
+    private void Update()
+    {
+        if (!HasStateAuthority) return; 
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ApplyRandomDamage();
+            string nick = Player.ConnectedPlayers[Random.Range(0, 4)].BasicStat.nickName;
+            RPC_ShowNotifyText(nick);
+            victoryCheck.CheckVictoryConditions();
+        }
+    }
+   
+   
+    [Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.All)]
+    private void RPC_ShowNotifyText(string nickname)
+    {
+        NotifyText.text = $"{nickname} 피해를 입었습니다 ";
+        NotifyText.gameObject.SetActive(true);
+    }
+   
+    private void ApplyRandomDamage()
+    {
+        List<Player> players = Player.ConnectedPlayers;
+   
+        if (players.Count == 0) return;
+   
+        // 살아있는 플레이어만 필터링
+        List<Player> alivePlayers = players.FindAll(p => !p.InGameStat.IsDead);
+   
+        if (alivePlayers.Count == 0)
+        {
+            Debug.Log("모든 플레이어가 사망했습니다.");
+            return;
+        }
+   
+        // 무작위 플레이어 선택
+        int randomIndex = Random.Range(0, alivePlayers.Count);
+        Player selectedPlayer = alivePlayers[randomIndex];
+   
+        // 데미지 적용
+        selectedPlayer.InGameStat.hp--;
+   
+        Debug.Log($"{selectedPlayer.BasicStat.nickName} 이(가) 데미지를 입었습니다. 남은 체력: {selectedPlayer.InGameStat.hp}");
+   
+        // 사망 판정
+        if (selectedPlayer.InGameStat.hp <= 0)
+        {
+            selectedPlayer.InGameStat.hp = 0;
+            Debug.Log($"{selectedPlayer.BasicStat.nickName} 이(가) 사망했습니다.");
+        }
+    }
 }
 
--- Assets/3.Script/Manager/VictoryCheck.cs
+++ Assets/3.Script/Manager/VictoryCheck.cs
@@ -5,91 +5,93 @@ using UnityEngine;
 
 public class VictoryCheck : MonoBehaviour
 {
-    // [SerializeField] private GameObject gameResultUI;
-    //
-    // [SerializeField] private TMP_Text gameResultText;
-    // [SerializeField] private TMP_Text[] playerTexts;
-    //
-    // public string gameResult;
-    //
-    // public void CheckVictoryConditions()
-    // {
-    //     string[] playerInfos = new string[4]; // 플레이어 순서 보안관/무법자/무법자/배신자
-    //     int outlawIndex = 1;
-    //
-    //     foreach (var player in GameManager.Instance.players)
-    //     {
-    //         string nickname = player.BasicStat.nickName;
-    //         string human = player.GameStat.InGameStat.MyHuman.Name;
-    //         string job = player.GameStat.InGameStat.MyJob.Name;
-    //         string info = $"{nickname}\n{human}\n{job}";
-    //
-    //         if (job == "보안관") playerInfos[0] = info;
-    //         else if (job == "무법자" && outlawIndex <= 2) playerInfos[outlawIndex++] = info;
-    //         else if (job == "배신자") playerInfos[3] = info;
-    //     }
-    //     
-    //     string result = GetGameResult();
-    //     if (string.IsNullOrEmpty(result)) return;
-    //     
-    //     Broadcaster.Instance.RPC_ShowResult(result, playerInfos);
-    // }
-    // private string GetGameResult()
-    // {
-    //     List<Player> players = GameManager.Instance.players;
-    //     Player sheriff = null;
-    //     List<Player> outlaws = new();
-    //     Player renegade = null;
-    //
-    //     foreach (var player in players)
-    //     {
-    //         var jobName = player.GameStat.InGameStat.MyJob.Name;
-    //         if (jobName == "보안관") sheriff = player;
-    //         else if (jobName == "무법자") outlaws.Add(player);
-    //         else if (jobName == "배신자") renegade = player;
-    //     }
-    //
-    //     if (sheriff != null && !sheriff.GameStat.InGameStat.IsDead)
-    //     {
-    //         bool allEnemiesDead = outlaws.TrueForAll(o => o.GameStat.InGameStat.IsDead);
-    //         if (renegade != null && !renegade.GameStat.InGameStat.IsDead)
-    //             allEnemiesDead = false;
-    //
-    //         if (allEnemiesDead)
-    //             return "sheriff is win!";
-    //     }
-    //
-    //     if (sheriff != null && sheriff.GameStat.InGameStat.IsDead)
-    //     {
-    //         if (outlaws.Exists(o => !o.GameStat.InGameStat.IsDead))
-    //             return "outlaw is win!";
-    //     }
-    //
-    //     int aliveCount = 0;
-    //     Player lastAlive = null;
-    //     foreach (var p in players)
-    //     {
-    //         if (!p.GameStat.InGameStat.IsDead)
-    //         {
-    //             aliveCount++;
-    //             lastAlive = p;
-    //         }
-    //     }
-    //
-    //     if (aliveCount == 1 && lastAlive == renegade)
-    //         return "renegade is win!";
-    //
-    //     return ""; // 아직 승리 조건이 안 됨
-    // }
-    //
-    // public void OpenGameResultUI(string[] playerInfos)
-    // {
-    //     gameResultUI.SetActive(true);
-    //     gameResultText.text = gameResult;
-    //
-    //     for (int i = 0; i < playerTexts.Length; i++)
-    //     {
-    //         playerTexts[i].text = playerInfos[i]; // 사전에 넘겨 받은 텍스트로 설정
-    //     }
-    // }
+     [SerializeField] private GameObject gameResultUI;
+    
+     [SerializeField] private TMP_Text gameResultText;
+     [SerializeField] private TMP_Text[] playerTexts;
+    
+     public string gameResult;
+    
+     public void CheckVictoryConditions()
+     {
+         string[] playerInfos = new string[4]; // 플레이어 순서 보안관/무법자/무법자/배신자
+         int outlawIndex = 1;
+    
+         foreach (var player in Player.Localplayer.ConnectedPlayers)
+         {
+              
+                 
+             string nickname = player.BasicStat.nickName;
+             string human = player.InGameStat.MyHuman.Name;
+             string job = player.InGameStat.MyJob.Name;
+             string info = $"{nickname}\n{human}\n{job}";
+    
+             if (job == "보안관") playerInfos[0] = info;
+             else if (job == "무법자" && outlawIndex <= 2) playerInfos[outlawIndex++] = info;
+             else if (job == "배신자") playerInfos[3] = info;
+         }
+         
+         string result = GetGameResult();
+         if (string.IsNullOrEmpty(result)) return;
+         
+         //Broadcaster.Instance.RPC_ShowResult(result, playerInfos);
+     }
+     private string GetGameResult()
+     {
+         List<Player> players = Player.ConnectedPlayers;
+         Player sheriff = null;
+         List<Player> outlaws = new();
+         Player renegade = null;
+    
+         foreach (var player in players)
+         {
+             var jobName = player.InGameStat.MyJob.Name;
+             if (jobName == "보안관") sheriff = player;
+             else if (jobName == "무법자") outlaws.Add(player);
+             else if (jobName == "배신자") renegade = player;
+         }
+    
+         if (sheriff != null && !sheriff.InGameStat.IsDead)
+         {
+             bool allEnemiesDead = outlaws.TrueForAll(o => o.InGameStat.IsDead);
+             if (renegade != null && !renegade.InGameStat.IsDead)
+                 allEnemiesDead = false;
+    
+             if (allEnemiesDead)
+                 return "sheriff is win!";
+         }
+    
+         if (sheriff != null && sheriff.InGameStat.IsDead)
+         {
+             if (outlaws.Exists(o => !o.InGameStat.IsDead))
+                 return "outlaw is win!";
+         }
+    
+         int aliveCount = 0;
+         Player lastAlive = null;
+         foreach (var p in players)
+         {
+             if (!p.InGameStat.IsDead)
+             {
+                 aliveCount++;
+                 lastAlive = p;
+             }
+         }
+    
+         if (aliveCount == 1 && lastAlive == renegade)
+             return "renegade is win!";
+    
+         return ""; // 아직 승리 조건이 안 됨
+     }
+    
+     public void OpenGameResultUI(string[] playerInfos)
+     {
+         gameResultUI.SetActive(true);
+         gameResultText.text = gameResult;
+    
+         for (int i = 0; i < playerTexts.Length; i++)
+         {
+             playerTexts[i].text = playerInfos[i]; // 사전에 넘겨 받은 텍스트로 설정
+         }
+     }
 }
