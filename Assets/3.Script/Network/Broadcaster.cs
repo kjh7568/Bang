e777d9776b3c804a6e096dd4e44c9ff58d455684
yunnozee <74864602+sksdbswl@users.noreply.github.com>
2025-05-29 11:02:14 +0900

    뱅, 맥주 사운드 적용

--- Assets/3.Script/Network/Broadcaster.cs
+++ Assets/3.Script/Network/Broadcaster.cs
@@ -226,10 +226,9 @@ public class Broadcaster : NetworkBehaviour
         Animator targetAnimator =target.GetComponent<Animator>();
         Debug.Log("miss : "+ attacker.name + " : " + target.name + " : " );
         attackerAnimator.SetTrigger("shooting");
-        SoundManager.Instance.PlaySound(SoundType.Bang);
-        
         targetAnimator.SetTrigger("hitting");
-        SoundManager.Instance.PlaySound(SoundType.Dodging);
+        
+        SoundManager.Instance.PlaySound(SoundType.Bang);
 
         Server.Instance.MovePlayersToSpawnPoints(GameManager.Instance.spawnPoints);
         //애니메이션
@@ -266,6 +265,9 @@ public class Broadcaster : NetworkBehaviour
         Player player = Player.GetPlayer(playerRef);
         Animator playerAnimator = player.GetComponent<Animator>();
         playerAnimator.SetTrigger("drinking");
+        
+        SoundManager.Instance.PlaySound(SoundType.Beer);
+        
         Server.Instance.MovePlayersToSpawnPoints(GameManager.Instance.spawnPoints);
         //애니메이션
         if (Runner.IsServer && Runner.LocalPlayer != playerRef)
