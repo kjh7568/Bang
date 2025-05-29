f6cc85817fec7e210c9c20a553e4174744d723f6
KimJeeHoon <rwrwg159@gmail.com>
2025-05-29 11:38:59 +0900

    맥주 체력 제한 직업 별로 다르게 설정

--- Assets/3.Script/Manager/CardSystem.cs
+++ Assets/3.Script/Manager/CardSystem.cs
@@ -144,7 +144,10 @@ public class CardSystem : MonoBehaviour
 
     private void UseBeer(PlayerRef user, PlayerRef? target)
     {
-        if (Player.GetPlayer(user).InGameStat.hp >= 5)
+        int currentHp = Player.GetPlayer(user).InGameStat.hp;
+        int maxHp = Player.LocalPlayer.InGameStat.MyJob.Name == "보안관" ? 5 : 4;
+
+        if (currentHp >= maxHp)
         {
             Debug.Log("풀피인데 맥주를 먹으면 안되죠...");
             return;
